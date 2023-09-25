4262981477c402b64a2bf0d53f12e6a6acee9c05
pilarxyz <pilar@student.uns.ac.id>
2023-09-25 21:40:33 +0700

    otp verify custom text

--- src/controllers/otpController.ts
+++ src/controllers/otpController.ts
@@ -1,19 +1,19 @@
-import { Request, Response } from 'express';
-import { z } from 'zod';
-import * as whatsappService from '../services/whatsappService';
-import * as db from '../db/db';
-import * as ResponseUtil from '../utils/response';
+import { Request, Response } from "express";
+import { z } from "zod";
+import * as whatsappService from "../services/whatsappService";
+import * as db from "../db/db";
+import * as ResponseUtil from "../utils/response";
 
-import crypto from 'crypto';
-import { handleSchemaValidation } from '../utils/schema-validation';
+import crypto from "crypto";
+import { handleSchemaValidation } from "../utils/schema-validation";
 
 function generateNumericOTP(length: number, salt: string): string {
-  const chars = '0123456789';
+  const chars = "0123456789";
   const otp = [];
   const randomBytes = crypto.randomBytes(length);
 
   // Combine the salt with random bytes for added security
-  const saltedBytes = Buffer.from(salt, 'utf-8');
+  const saltedBytes = Buffer.from(salt, "utf-8");
   for (let i = 0; i < length; i++) {
     randomBytes[i] ^= saltedBytes[i % saltedBytes.length];
   }
@@ -23,7 +23,7 @@ function generateNumericOTP(length: number, salt: string): string {
     otp.push(chars.charAt(index));
   }
 
-  return otp.join('');
+  return otp.join("");
 }
 
 export const generate = async (req: Request, res: Response) => {
@@ -48,12 +48,12 @@ export const generate = async (req: Request, res: Response) => {
   const otp = generateNumericOTP(6, data.receiver);
 
   let customText;
-  if (data.message.includes('{otp}')) {
-    customText = data.message.replace('{otp}', otp.toString());
+  if (data.message.includes("{otp}")) {
+    customText = data.message.replace("{otp}", otp.toString());
   } else {
     return ResponseUtil.badRequest({
       res,
-      message: 'Message should contain {otp} placeholder.',
+      message: "Message should contain {otp} placeholder.",
     });
   }
 
@@ -84,7 +84,7 @@ export const generate = async (req: Request, res: Response) => {
         formattedMessage,
         1000
       );
-      return ResponseUtil.ok({ res, data: result, message: 'Message sent' });
+      return ResponseUtil.ok({ res, data: result, message: "Message sent" });
     } catch (error) {
       // Handle the error if needed
       console.error(
@@ -102,6 +102,7 @@ export const verify = async (req: Request, res: Response) => {
     sender: z.string(),
     receiver: z.string(),
     otp: z.string(),
+    message: z.string().optional(),
   });
 
   const data = handleSchemaValidation(messageSchema, req.body, res);
@@ -110,7 +111,6 @@ export const verify = async (req: Request, res: Response) => {
     return;
   }
 
-  // insert otp and receiver to db
   const session = await whatsappService.getSessionAndCheckStatus(
     data.sender,
     res
@@ -121,13 +121,12 @@ export const verify = async (req: Request, res: Response) => {
   }
 
   try {
-    console.log('session', session);
     // check if user exists
     const users = await db.getUser(data.receiver);
     if (users.length === 0) {
       return ResponseUtil.badRequest({
         res,
-        message: 'User not found',
+        message: "User not found",
       });
     }
 
@@ -139,35 +138,32 @@ export const verify = async (req: Request, res: Response) => {
     if (!isValid) {
       return ResponseUtil.badRequest({
         res,
-        message: 'Invalid OTP',
+        message: "Invalid OTP",
       });
     }
 
     const now = new Date();
     const offset = now.getTimezoneOffset() * 60 * 1000;
     const timestamp = new Date(user.timestamp).getTime() - offset;
-
     const diff = now.getTime() - timestamp;
 
     // check if otp is expired (5 minutes)
     if (diff > 5 * 60 * 1000) {
       return ResponseUtil.badRequest({
         res,
-        message: 'OTP expired',
+        message: "OTP expired",
       });
     }
 
     // delete user from db
     await db.deleteUser(data.receiver);
 
-    const text = `Login berhasil pada ${now.toISOString()}`;
-
-    try {
+    // Only proceed if message is provided in the request body
+    if (data.message) {
       let formattedMessage = {
-        text: text,
+        text: data.message,
       } as any;
 
-      console.log(formattedMessage);
       const formattedPhoneNumber = whatsappService.formatPhone(data.receiver);
       try {
         const result = await whatsappService.sendMessage(
@@ -178,8 +174,8 @@ export const verify = async (req: Request, res: Response) => {
         );
         return ResponseUtil.ok({
           res,
-          data: { isValid, result },
-          message: 'OTP verified',
+          data: { messageSent: true, result },
+          message: "OTP verified",
         });
       } catch (error) {
         // Handle the error if needed
@@ -187,9 +183,13 @@ export const verify = async (req: Request, res: Response) => {
           `Failed to send message to recipient ${data.receiver}: ${error}`
         );
       }
-    } catch (error) {
-      console.log(error);
-      return ResponseUtil.internalError({ res, err: error });
+    } else {
+      // If no message provided, just send a response without sending a message
+      return ResponseUtil.ok({
+        res,
+        data: { messageSent: false },
+        message: "OTP verified",
+      });
     }
   } catch (error) {
     console.log(error);
